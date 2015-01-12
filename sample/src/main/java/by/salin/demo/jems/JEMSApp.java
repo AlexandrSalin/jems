7bd415197cf7d57fc5b9af54cfa5b09fe63b9af1
Alexandr.Salin <Salin.Alexandr@gmail.com>
2015-01-13 02:09:33 +0300

    config for deploy to maven central

--- sample/src/main/java/by/salin/demo/jems/JEMSApp.java
+++ sample/src/main/java/by/salin/demo/jems/JEMSApp.java
@@ -3,20 +3,30 @@ package by.salin.demo.jems;
 import android.app.Application;
 import android.content.Context;
 
+import by.salin.apps.jems.EventHandlerCallback;
+import by.salin.apps.jems.JEMS;
+import by.salin.apps.jems.impl.Event;
+
 /**
  * App instance.
  * Created by Alexandr.Salin on 1/3/15.
  */
-public class JEMSApp extends Application {
+public class JEMSApp extends Application implements EventHandlerCallback {
     private static JEMSApp instance;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
+        JEMS.dispatcher().removeListener(this);
     }
 
     public static Context getInstance() {
         return instance.getApplicationContext();
+    }
+
+    @Override
+    public void onEvent(Event event) {
+
     }
 }
